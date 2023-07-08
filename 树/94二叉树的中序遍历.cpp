d092c566e71e42f24c6e13308d60a74f79b1f82c
booven <76235948+booven@users.noreply.github.com>
2023-07-08 12:48:03 +0800

    Update 94二叉树的中序遍历.cpp

--- 树/94二叉树的中序遍历.cpp
+++ 树/94二叉树的中序遍历.cpp
@@ -1,3 +1,7 @@
+原题链接：https://leetcode.cn/problems/binary-tree-inorder-traversal/
+
+思路：递归
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -12,16 +16,50 @@
 class Solution {
 public:
     vector<int> res;
-    void LDR(TreeNode* node) {
-        if (node == nullptr) {
+
+    void LDR(TreeNode* root) {  //中序遍历递归常规写法
+        if (root == nullptr) {
             return;
-        } 
-        LDR(node->left);
-        res.push_back(node->val);
-        LDR(node->right);
+        }
+        LDR(root->left);
+        res.push_back(root->val);
+        LDR(root->right);
     }
+
     vector<int> inorderTraversal(TreeNode* root) {
         LDR(root);
+        return res;
+    }
+};
+
+思路：迭代
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> res;
+        stack<TreeNode*> stk;
+        while (root != nullptr || !stk.empty()) {
+            while (root != nullptr) {
+                stk.push(root);
+                root = root->left;
+            }
+            root = stk.top();
+            stk.pop();
+            res.push_back(root->val);
+            root = root->right;
+        }
         return res;
     }
 };
